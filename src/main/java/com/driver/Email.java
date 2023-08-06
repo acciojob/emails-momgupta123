8244fee1d988fbf47a8ec89ee7bec2f216e64612
momgupta123 <momgupta06@gmail.com>
2023-08-07 04:40:17 +0530

    hello

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -5,7 +5,7 @@ public class Email {
     private String emailId;
     private String password;
 
-    public Email(String emailId){
+    public Email(String emailId) {
         this.emailId = emailId;
         this.password = "Accio@123";
     }
@@ -18,12 +18,25 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
-        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+    public void changePassword(String oldPassword, String newPassword) {
+        // Change password only if the oldPassword is equal to the current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        // Check if the oldPassword matches the current password
+        if (oldPassword.equals(password)) {
+            // Check if the newPassword meets all the conditions
+            if (newPassword.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")) {
+                this.password = newPassword;
+                System.out.println("Password changed successfully!");
+            } else {
+                System.out.println("Invalid new password! Please make sure it meets all the conditions.");
+            }
+        } else {
+            System.out.println("Incorrect old password! Password not changed.");
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,30 +1,42 @@
 package com.driver;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
 
-public class Workspace extends Gmail{
-
-    private ArrayList<Meeting> calendar; // Stores all the meetings
+public class Workspace extends Gmail {
+    private ArrayList<Meeting> calendar;
 
     public Workspace(String emailId) {
-        // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId, Integer.MAX_VALUE);
+        calendar = new ArrayList<>();
     }
 
-    public void addMeeting(Meeting meeting){
-        //add the meeting to calendar
-
+    public void addMeeting(Meeting meeting) {
+        calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
-        // find the maximum number of meetings you can attend
-        // 1. At a particular time, you can be present in at most one meeting
-        // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
-        // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+    public int findMaxMeetings() {
+        int maxMeetings = 0;
+        int currentMeetings = 0;
+        Collections.sort(calendar, Comparator.comparing(Meeting::getStartTime));
 
+        ArrayList<Meeting> meetingsToRemove = new ArrayList<>();
+        for (int i = 0; i < calendar.size(); i++) {
+            Meeting meeting = calendar.get(i);
+            while (i + 1 < calendar.size() && meeting.getEndTime().isAfter(calendar.get(i + 1).getStartTime())) {
+                currentMeetings++;
+                meetingsToRemove.add(calendar.get(i + 1));
+                i++;
+            }
+            if (currentMeetings > maxMeetings) {
+                maxMeetings = currentMeetings;
+            }
+            currentMeetings--;
+        }
+
+        calendar.removeAll(meetingsToRemove);
+        return maxMeetings;
     }
 }
